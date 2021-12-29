ecd3883b35dfa9b8c43c2c6f35b4ea4cca9c2978
yayaveli <yabadji2010@gmail.com>
2021-12-29 01:07:12 +0000

    Add Authentication and Authorization with JWT

---
+++ src/main/java/org/sid/secservice/security/JWTAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package org.sid.secservice.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.sid.secservice.entities.AppUser;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+AuthenticationManager authenticationManager;
+
+public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+}
+@Override
+public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
+    try {
+        AppUser appUser = null;
+        appUser = new ObjectMapper().readValue(request.getInputStream(), AppUser.class);
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(appUser.getUsername(), appUser.getPassword()));
+    } catch (JsonParseException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+
+
+}
+
+@Override
+protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication authResult) throws IOException, ServletException {
+    User user = (User) authResult.getPrincipal();
+    List<String> roles = new ArrayList<>();
+    authResult.getAuthorities().forEach(a->{
+        roles.add(a.getAuthority());
+    });
+
+    String jwt = JWT.create()
+                .withIssuer(request.getRequestURI())
+                .withSubject(user.getUsername())
+                .withArrayClaim("roles", roles.toArray(new String[roles.size()]))
+                .withExpiresAt(new Date(System.currentTimeMillis()+SecurityParams.EXPIRATION))
+                .sign(Algorithm.HMAC256(SecurityParams.SECRET));
+
+                response.addHeader(SecurityParams.JWT_HEADER_NAME, jwt);
+
+}
+
+}
